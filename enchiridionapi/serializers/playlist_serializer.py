337f4eb275cb70375ea326bba081dde4c9625566
Neil MacLean <nbmac13@gmail.com>
2024-01-18 17:17:59 -0600

    new like functionality and cookie improvements

--- enchiridionapi/serializers/playlist_serializer.py
+++ enchiridionapi/serializers/playlist_serializer.py
@@ -6,15 +6,22 @@ from enchiridionapi.serializers import LocalEpisodeSerializer
 class PlaylistSerializer(serializers.ModelSerializer):
     episodes = serializers.SerializerMethodField()
     likes_count = serializers.IntegerField(read_only=True)
+    is_liked = serializers.SerializerMethodField()
 
     class Meta:
         model = Playlist
-        fields = ['id', 'user_id', 'name', 'description', 'episodes', 'likes_count']
+        fields = ['id', 'user_id', 'name', 'description', 'episodes', 'likes_count', 'is_liked']
 
     def get_episodes(self, obj):
         episode_playlist = obj.playlist_episodes
         serializer = LocalEpisodeSerializer([ep.episode for ep in episode_playlist], many=True)
         return serializer.data
+    
+    def get_is_liked(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return obj.like_set.filter(user=user).exists()
+        return False
 
     @staticmethod
     def setup_eager_loading(queryset):
